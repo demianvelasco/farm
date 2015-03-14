ffb904ee715157e15b9cba4e403805207a6239e8
Alejandro Demian Velasco <demianvelasco@gmail.com>
2015-03-14 13:56:07 -0400

    Added sensors

--- atmega.c
+++ atmega.c
@@ -1,6 +1,14 @@
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
+#include "DHT.h"
+
+#define THERM0_PIN 0
+#define THERM1_PIN 1
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 int fsrPin = 0;
 
@@ -30,10 +38,12 @@ RF24 radio(9,10);
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL,0xF0F0F0F0D2LL };
 
 // here we can send up to 30 chars
-char SendPayload[31] = "FARM is the way to do it";
+char SendPayload[31] = "FARM module";
 
 void setup(void) {
   Serial.begin(57600); //Debug 
+  dht.begin(); 
+  
   printf_begin();
   //nRF24 configurations
   radio.begin();
@@ -51,9 +61,11 @@ void setup(void) {
 void loop() {
   
   //Get temperature from sensor
-  float temperature = getTemp();
+  float moisture = getMoisture();
+  
+  Serial.println(moisture);
   // Assign temperature to payload, here am sending it as string
-  dtostrf(temperature,2,2,SendPayload);
+  dtostrf(moisture,2,2,SendPayload);
   
   //add a tag
   //strcat(SendPayload, "X");   // add first string
@@ -66,6 +78,20 @@ void loop() {
 
   // slow down a bit
   delay(1000);  
+  
+  
+  // HDT11
+  delay(2000);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht.readTemperature(true);
+  
+  
+  
 }
 
 
@@ -77,5 +103,21 @@ float getTemp(){
   TemperatureSum = 8;//analogRead(fsrPin);
   
   return TemperatureSum;
+  
+}
+
+float getGroundTemp(){
+  float temperature = 0;
+  temperature = analogRead(THERM1_PIN) - 238;
+  delay(50);
+  return temperature;
+  
+}
+
+float getMoisture(){
+  float moisture = 0;
+  moisture = analogRead(THERM0_PIN);
+  delay(50);
+  return moisture;
   
 }
