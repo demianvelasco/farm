3f6eb1ec52dfd6aa6c71e0c136954f766d2df341
Demian Velasco <demian@Demians-MacBook-Air.local>
2015-04-23 05:48:30 -0400

    added data charts

--- WebApp/js/app.js
+++ WebApp/js/app.js
@@ -146,32 +146,104 @@ app.controller('dataController', ['$scope', function($scope) {
 
 	$scope.selectedItem = $scope.items[0];
 
+        // Day time array
+        var rowData1 = [['Time', 'Ground Temp 1', 'Ground Temp 2', 'Ground Temp 3', 'Temp', 'Humidity', 'Light'],
+                        ['9 am',             165,             938,             522,    998,        450,   114.6],
+                        ['11 am',            135,            1120,             599,   1268,        288,     382],
+                        ['1 pm',             157,            1167,             587,    807,        397,     623],
+                        ['3 pm',             139,            1110,             615,    968,        215,   409.4],
+                        ['5 pm',             136,             691,             629,   1026,        366,   569.6]];
+        
+        var rowData2 = [['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua  Guinea',
+        'Rwanda', 'Average'],
+        ['2004/05', 122, 638, 722, 998, 450, 614.6],
+        ['2005/06', 100, 1120, 899, 1268, 288, 682],
+        ['2006/07', 183, 167, 487, 207, 397, 623],
+        ['2007/08', 200, 510, 315, 1068, 215, 609.4],
+        ['2008/09', 123, 491, 829, 826, 366, 569.6]];
+
+    // Create and populate the data tables.
+    var data = [];
+    data[0] = google.visualization.arrayToDataTable(rowData1);
+    data[1] = google.visualization.arrayToDataTable(rowData2);
+    data[2] = google.visualization.arrayToDataTable(rowData1);
+    data[3] = google.visualization.arrayToDataTable(rowData2);
+    data[4] = google.visualization.arrayToDataTable(rowData1);
+
+    console.log(data[0]);
+
+    var options = {
+      width: 750,
+      height: 390,
+      vAxis: {title: "Cups"},
+      hAxis: {title: "Month"},
+      curveType: 'function',
+      series: {5: {type: "line"}},
+      animation:{
+        duration: 1000,
+        easing: 'out'
+    },
+};
+var current = 0;
+    // Create and draw the visualization.
+    var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+    var button = document.getElementById('b1');
+    function drawChart() {
+      // Disabling the button while the chart is drawing.
+      button.disabled = true;
+      google.visualization.events.addListener(chart, 'ready',
+          function() {
+            button.disabled = false;
+            button.value = 'Switch to ' + (current ? 'A' : 'B');
+        });
+    //  options['title'] = 'Module ' + (current ? 'A' : 'B' : 'C' : 'D': 'E') + ' Production by Country';
+      options['title'] = 'Module ' + (current ? 'A' : 'B') + ' Production by Country';
+
+      chart.draw(data[current], options);
+  }
+  drawChart();
+
+  button.onclick = function() {
+      current = 1 - current;
+      drawChart();
+  }
+
     // Selector Function
     $scope.getChart = function () {
       switch($scope.selectedItem.id) {
          case 1:
          {
             console.log("You selected one");
+            current = 0;
+            drawChart();
         }
         break;
         case 2:
         {
             console.log("You selected two");
+            current = 1;
+            drawChart();
         }
         break;
         case 3:
         {
             console.log("You selected three");
+            current = 2;
+            drawChart();
         }
         break;
         case 4:
         {
             console.log("You selected four");
+            current = 3;
+            drawChart();
         }
         break;
         case 5:
         {
             console.log("You selected five");
+            current = 4;
+            drawChart();
         }
         break;
         default:
@@ -183,51 +255,6 @@ app.controller('dataController', ['$scope', function($scope) {
     console.log($scope.selectedItem.id);
 
 };
-	google.setOnLoadCallback(drawChart)
-
-  function drawChart() {
-     var data = google.visualization.arrayToDataTable([
-        ['Module', 'Sales', 'Expenses'],
-        ['2004',  1000,      400],
-        ['2005',  1170,      460],
-        ['2006',  660,       1120],
-        ['2007',  1030,      540]
-        ]);
-
-     var options = {
-        title: 'Company Performance',
-        curveType: 'function',
-        legend: { position: 'bottom' },
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-    chart.draw(data, options);
-}
-
-google.setOnLoadCallback(drawChart2)
-
-function drawChart2() {
- var data = google.visualization.arrayToDataTable([
-    ['Module ', 'Sales', 'Expenses'],
-    ['2004',  1000,      400],
-    ['2005',  1170,      460],
-    ['2006',  660,       1120],
-    ['2007',  1030,      540]
-    ]);
-
- var options = {
-    title: 'Company Performance',
-    curveType: 'function',
-    legend: { position: 'bottom' }
-};
-
-var chart = new google.visualization.LineChart(document.getElementById('chart_div2'));
-
-chart.draw(data, options);
-}
-
-
 
 
 }]);
